03d52db6443205b9b93064a52e7f21ab582d9a51
Lamine DIEME <lamine.dieme@gainde2000.sn>
2024-07-11 09:30:23 +0000

    Update tabs design

--- src/app/reporting/transactions/transactions.component.ts
+++ src/app/reporting/transactions/transactions.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.css']
+  styleUrls: ['./transactions.component.css'],
 })
-export class TransactionsComponent implements OnInit {
+export class TransactionsComponent implements OnInit, AfterViewInit {
+  @ViewChild('navTab', { static: false }) navTab!: ElementRef;
+  showLeftBtn = false;
+  showRightBtn = false;
 
   constructor(public dialog: MatDialog) {}
 
@@ -16,21 +19,127 @@ export class TransactionsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.updateScrollButtons();
   }
 
-  tablist: { img: string, title: string, controls: string, selected: boolean, id: string }[] = [
-    { img: 'assets/images/om.png', title: 'Orange Money', controls: 'nav-om', selected: true, id: 'nav-om-tab' },
-    { img: 'assets/images/wave.png', title: 'Wave', controls: 'nav-wave', selected: false, id: 'nav-wave-tab' },
-    { img: 'assets/images/ria.png', title: 'Ria', controls: 'nav-ria', selected: false, id: 'nav-ria-tab' },
-    { img: 'assets/images/airtel.png', title: 'Airtel', controls: 'nav-airtel', selected: false, id: 'nav-airtel-tab' },
-    { img: 'assets/images/airtel-mobile.png', title: 'Airtel Mobile', controls: 'nav-airtel-mobile', selected: false, id: 'nav-airtel-mobile-tab' },
-    { img: 'assets/images/wizall.png', title: 'Wizall', controls: 'nav-wizall', selected: false, id: 'nav-wizall-tab' },
-    { img: 'assets/images/yoome.png', title: 'Yoome', controls: 'nav-yoome', selected: false, id: 'nav-yoome-tab' },
-    { img: 'assets/images/tailmob.png', title: 'Tailmob', controls: 'nav-tailmob', selected: false, id: 'nav-tailmob-tab' },
-    { img: 'assets/images/senelec.png', title: 'Senelec', controls: 'nav-senelec', selected: false, id: 'nav-senelec-tab' },
-    { img: 'assets/images/starrtimes.png', title: 'Starrtimes', controls: 'nav-starrtimes', selected: false, id: 'nav-starrtimes-tab' },
-    { img: 'assets/images/freemoney.png', title: 'Freemoney', controls: 'nav-freemoney', selected: false, id: 'nav-freemoney-tab' },
-    { img: 'assets/images/kash.png', title: 'Kash', controls: 'nav-kash', selected: false, id: 'nav-kash-tab' }
-  ]
+  ngAfterViewInit() {
+    this.updateScrollButtons();
+  }
 
+  tablist: {
+    img: string;
+    title: string;
+    controls: string;
+    selected: boolean;
+    id: string;
+  }[] = [
+    {
+      img: 'assets/images/om.png',
+      title: 'Orange Money',
+      controls: 'nav-om',
+      selected: true,
+      id: 'nav-om-tab',
+    },
+    {
+      img: 'assets/images/wave.png',
+      title: 'Wave',
+      controls: 'nav-wave',
+      selected: false,
+      id: 'nav-wave-tab',
+    },
+    {
+      img: 'assets/images/ria.png',
+      title: 'Ria',
+      controls: 'nav-ria',
+      selected: false,
+      id: 'nav-ria-tab',
+    },
+    {
+      img: 'assets/images/airtel.png',
+      title: 'Airtel',
+      controls: 'nav-airtel',
+      selected: false,
+      id: 'nav-airtel-tab',
+    },
+    {
+      img: 'assets/images/airtel-mobile.png',
+      title: 'Airtel Mobile',
+      controls: 'nav-airtel-mobile',
+      selected: false,
+      id: 'nav-airtel-mobile-tab',
+    },
+    {
+      img: 'assets/images/wizall.png',
+      title: 'Wizall',
+      controls: 'nav-wizall',
+      selected: false,
+      id: 'nav-wizall-tab',
+    },
+    {
+      img: 'assets/images/yoome.png',
+      title: 'Yoome',
+      controls: 'nav-yoome',
+      selected: false,
+      id: 'nav-yoome-tab',
+    },
+    {
+      img: 'assets/images/tailmob.png',
+      title: 'Tailmob',
+      controls: 'nav-tailmob',
+      selected: false,
+      id: 'nav-tailmob-tab',
+    },
+    {
+      img: 'assets/images/senelec.png',
+      title: 'Senelec',
+      controls: 'nav-senelec',
+      selected: false,
+      id: 'nav-senelec-tab',
+    },
+    {
+      img: 'assets/images/starrtimes.png',
+      title: 'Starrtimes',
+      controls: 'nav-starrtimes',
+      selected: false,
+      id: 'nav-starrtimes-tab',
+    },
+    {
+      img: 'assets/images/freemoney.png',
+      title: 'Freemoney',
+      controls: 'nav-freemoney',
+      selected: false,
+      id: 'nav-freemoney-tab',
+    },
+    {
+      img: 'assets/images/kash.png',
+      title: 'Kash',
+      controls: 'nav-kash',
+      selected: false,
+      id: 'nav-kash-tab',
+    }
+  ];
+
+  scrollTabs(direction: 'left' | 'right') {
+    const tabsContainer = this.navTab.nativeElement;
+    const scrollAmount = 200; // Ajustez ceci selon vos besoins
+    const currentScroll = tabsContainer.scrollLeft;
+
+    if (direction === 'left') {
+      tabsContainer.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    } else if (direction === 'right') {
+      tabsContainer.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
+
+    setTimeout(() => this.updateScrollButtons(), 300); // Update buttons after the scroll animation
+  }
+
+  updateScrollButtons() {
+    if (this.navTab && this.navTab.nativeElement) {
+      const tabsContainer = this.navTab.nativeElement;
+      this.showLeftBtn = tabsContainer.scrollLeft > 0;
+      this.showRightBtn =
+        tabsContainer.scrollWidth >
+        tabsContainer.scrollLeft + tabsContainer.clientWidth;
+    }
+  }
 }
