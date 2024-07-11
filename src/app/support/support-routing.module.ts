ad43ef4c887897c47cacfb3d8135c8fa75e61fbd
Lamine DIEME <lamine.dieme@gainde2000.sn>
2024-07-11 08:50:49 +0000

    remove scroolbar

--- src/app/support/support-routing.module.ts
+++ src/app/support/support-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from '../core/main/main.component';
-import { DetailTicketingComponent } from './detail-ticketing/detail-ticketing.component';
 import { FaqComponent } from './faq/faq.component';
 import { FormationsComponent } from './formations/formations.component';
 import { TicketignsComponent } from './ticketigns/ticketigns.component';
@@ -13,7 +12,6 @@ const routes: Routes = [
     component: MainComponent,
     children: [
       { path: 'ticketings', component: TicketignsComponent },
-      { path: 'detail/ticketings', component: DetailTicketingComponent },
       { path: 'formations', component: FormationsComponent },
       { path: 'faq', component: FaqComponent },
     ]
--- src/app/support/support.module.ts
+++ src/app/support/support.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { SupportRoutingModule } from './support-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { MaterialUiModule } from '../shared/material-ui/material-ui.module';
 import { TicketignsComponent } from './ticketigns/ticketigns.component';
-import { DetailTicketingComponent } from './detail-ticketing/detail-ticketing.component';
 import { FormationsComponent } from './formations/formations.component';
 import { FaqComponent } from './faq/faq.component';
 import { CoreModule } from "../core/core.module";
@@ -14,7 +12,6 @@ import { CoreModule } from "../core/core.module";
 @NgModule({
     declarations: [
         TicketignsComponent,
-        DetailTicketingComponent,
         FormationsComponent,
         FaqComponent,
     ],
